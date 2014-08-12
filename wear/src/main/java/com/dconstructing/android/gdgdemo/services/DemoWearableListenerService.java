0001c14d7f30b62228dec2936b3414d93f79017c
David Cox <dcox@getjive.com>
2014-08-11 23:17:46 -0600

    Create and register the service

---
+++ wear/src/main/java/com/dconstructing/android/gdgdemo/services/DemoWearableListenerService.java
@@ -0,0 +1,10 @@
+package com.dconstructing.android.gdgdemo.services;
+
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * Created by dcox on 8/11/14.
+ */
+public class DemoWearableListenerService extends WearableListenerService {
+    // TODO: Implement the onPeerDisconnected method
+}
