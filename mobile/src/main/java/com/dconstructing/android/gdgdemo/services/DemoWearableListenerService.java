7aaed58885fb58c81d0486efaa006d2d28f41545
David Cox <dcox@getjive.com>
2014-08-12 22:52:46 -0600

    Handheld version of wearable listener service

---
+++ mobile/src/main/java/com/dconstructing/android/gdgdemo/services/DemoWearableListenerService.java
@@ -0,0 +1,12 @@
+package com.dconstructing.android.gdgdemo.services;
+
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * Created by dcox on 8/12/14.
+ */
+public class DemoWearableListenerService extends WearableListenerService {
+
+	// TODO: implement onMessageReceived method
+
+}
