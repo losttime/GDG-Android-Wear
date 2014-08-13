7e2f66601465519886ae384990b6596f296b9fda
David Cox <dcox@getjive.com>
2014-08-12 20:15:41 -0600

    Show disconnect notification

--- wear/src/main/java/com/dconstructing/android/gdgdemo/services/DemoWearableListenerService.java
+++ wear/src/main/java/com/dconstructing/android/gdgdemo/services/DemoWearableListenerService.java
@@ -1,10 +1,32 @@
 package com.dconstructing.android.gdgdemo.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import com.dconstructing.android.gdgdemo.R;
+import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.WearableListenerService;
 
 /**
  * Created by dcox on 8/11/14.
  */
 public class DemoWearableListenerService extends WearableListenerService {
-    // TODO: Implement the onPeerDisconnected method
+
+	@Override
+	public void onPeerDisconnected(Node peer) {
+		super.onPeerDisconnected(peer);
+
+		Notification notification = new Notification.Builder(this)
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("Warning")
+				.setContentText("You are disconnected")
+				.setPriority(Notification.PRIORITY_HIGH)
+				.setDefaults(Notification.DEFAULT_ALL)
+				.build();
+
+		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(1, notification);
+	}
+
 }
