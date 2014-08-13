4a05e53fb9b06716fc78ae002d4e325522f74240
David Cox <dcox@getjive.com>
2014-08-12 22:06:46 -0600

    Show notification on connect

--- wear/src/main/java/com/dconstructing/android/gdgdemo/services/DemoWearableListenerService.java
+++ wear/src/main/java/com/dconstructing/android/gdgdemo/services/DemoWearableListenerService.java
@@ -3,6 +3,7 @@ package com.dconstructing.android.gdgdemo.services;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 
 import com.dconstructing.android.gdgdemo.R;
 import com.google.android.gms.wearable.Node;
@@ -29,6 +30,22 @@ public class DemoWearableListenerService extends WearableListenerService {
 		notificationManager.notify(1, notification);
 	}
 
-	// TODO: Implement the onPeerConnected method
+	@Override
+	public void onPeerConnected(Node peer) {
+		super.onPeerConnected(peer);
+
+		// TODO: Add an action to this notification
+
+		Notification notification = new Notification.Builder(this)
+				.setSmallIcon(R.drawable.ic_launcher)
+				.setContentTitle("All Clear")
+				.setContentText("We're back ;)")
+				.setPriority(NotificationCompat.PRIORITY_DEFAULT)
+				.setDefaults(NotificationCompat.DEFAULT_ALL)
+				.build();
+
+		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(1, notification);
+	}
 
 }
